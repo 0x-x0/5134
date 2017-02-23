d5cd077c63239ae87cc7159c5ca80976b2acf4f2
chetantarale <chetan_tarale@yahoo.com>
2017-02-23 12:50:41 +0530

    lockNload

---
+++ lockNload.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+export TEAMID=$(echo $TEAMPARAMS_PARAMS_TMID | awk '{print toupper($0)}')
+export OWNERTOKEN=$(eval echo "$TEAMPARAMS_PARAMS_TOKEN")
+
+check_jq() {
+  {
+    type jq &> /dev/null && echo "jq is already installed"
+  } || {
+    echo "Installing 'jq'"
+    apt-get install -y jq
+  }
+}
+
+# getTeamId() {
+#   local teams=$(curl --silent -X GET -H "Accept: application/json" -H "Authorization: token $TOKEN" https://api.github.com/orgs/"$ORG"/teams)
+#   TEAMID=$(echo $teams |  jq ".[] | select(.name==\"$TEAMNAME\") | .id")
+# }
+
+getTeamRepos() {
+  local res=$(curl --silent -X GET -H "Accept: application/json" -H "Authorization: token $TOKEN" https://api.github.com/teams/$TEAMID/repos)
+  TEAMREPOS=$(echo $res |  jq ".[] | .name")
+}
+
+changePermissions() {
+  local permission="$1"
+  local data="{\"permission\": \"$permission\"}"
+  for repo in $TEAMREPOS; do
+    repoName=$(echo "$repo" | sed -e 's/^"//' -e 's/"$//')
+    url="https://api.github.com/teams/$TEAMID/repos/$ORG/$repoName"
+    local responseCode=$(curl --write-out %{http_code} --silent -X GET -H "Accept: application/json" -H "Authorization: token $TOKEN" $url)
+    if [ $responseCode -eq 204 ]; then
+      local res=$(curl --write-out %{http_code} --silent -X PUT -H "Content-Type: application/json" -H "Accept: application/vnd.github.v3.repository+json" -H "Authorization: token $TOKEN" $url -d "$data")
+      if [ $res -eq 204 ]; then
+        echo "Permission updated for $repoName"
+      fi
+    fi
+  done
+}
+
+main() {
+  check_jq
+  #getTeamId
+  getTeamRepos
+  changePermissions "$@"
+}
+
+main "$@"
